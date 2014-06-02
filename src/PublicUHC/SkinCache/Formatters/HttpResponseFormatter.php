4ea154c8871d873dc917bff2451aa98d086f8798
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-02 17:06:49 +0100

    return a response with the image data

--- src/PublicUHC/SkinCache/Formatters/HttpResponseFormatter.php
+++ src/PublicUHC/SkinCache/Formatters/HttpResponseFormatter.php
@@ -1,10 +1,12 @@
 <?php
 namespace PublicUHC\SkinCache\Formatters;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class HttpResponseFormatter extends Formatter {
 
     function format($data)
     {
-        // TODO: Implement format() method.
+        return new Response($data, 200,  ['content-type' => 'image/png']);
     }
 }
