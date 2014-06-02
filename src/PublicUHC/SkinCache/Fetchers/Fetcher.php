d6603c90931d9dd92189730fdf89ac398e4adae5
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-02 16:14:36 +0100

    the fetcher should have a cache

--- src/PublicUHC/SkinCache/Fetchers/Fetcher.php
+++ src/PublicUHC/SkinCache/Fetchers/Fetcher.php
@@ -5,16 +5,19 @@ namespace PublicUHC\SkinCache\Fetchers;
 
 use PublicUHC\SkinCache\Downloaders\Downloader;
 use PublicUHC\SkinCache\Formatters\Formatter;
+use Stash\Interfaces\PoolInterface;
 
 abstract class Fetcher {
 
     private $downloader;
     private $formatter;
+    private $cachePool;
 
-    public function __construct(Downloader $downloader, Formatter $formatter)
+    public function __construct(Downloader $downloader, Formatter $formatter, PoolInterface $cachePool)
     {
         $this->downloader = $downloader;
         $this->formatter = $formatter;
+        $this->cachePool = $cachePool;
     }
 
     abstract function fetchSkin($username);
