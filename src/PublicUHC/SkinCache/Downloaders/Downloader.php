f0e1ea1db2abb663dde3d97832a2996a787e6858
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-02 16:03:43 +0100

    docs

--- src/PublicUHC/SkinCache/Downloaders/Downloader.php
+++ src/PublicUHC/SkinCache/Downloaders/Downloader.php
@@ -2,6 +2,7 @@
 
 namespace PublicUHC\SkinCache\Downloaders;
 
+use PublicUHC\SkinCache\Exceptions\DownloadException;
 use Stash\Interfaces\PoolInterface;
 
 abstract class Downloader {
@@ -13,10 +14,25 @@ abstract class Downloader {
         $this->cachePool = $cache;
     }
 
+    /**
+     * @param $username String the username to fetch
+     * @return resource the skin data
+     * @throws DownloadException if fetching failed
+     */
     abstract function downloadSkin($username);
 
+    /**
+     * @param $username String the username to fetch
+     * @param $size int the dimensions of the square image
+     * @return resource the helm data
+     */
     abstract function downloadHelm($username, $size);
 
+    /**
+     * @param $username String the username to fetch
+     * @param $size int the dimensions of the square image
+     * @return resource the head data
+     */
     abstract function downloadHead($username, $size);
 
 } 
