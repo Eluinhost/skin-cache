04fe83cec38d6d442f56bbd0edbfa559e382669f
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-06 09:28:07 +0100

    return transparent images of the right size if there was an error

--- src/PublicUHC/SkinCache/SkinFetcher.php
+++ src/PublicUHC/SkinCache/SkinFetcher.php
@@ -4,7 +4,9 @@ namespace PublicUHC\SkinCache;
 
 
 use PublicUHC\SkinCache\Downloaders\Downloader;
+use PublicUHC\SkinCache\Exceptions\DownloadException;
 use PublicUHC\SkinCache\Formatters\Formatter;
+use PublicUHC\SkinCache\Painters\TransparentImagePainter;
 use Stash\Interfaces\PoolInterface;
 
 class SkinFetcher {
@@ -12,15 +14,31 @@ class SkinFetcher {
     private $downloader;
     private $formatter;
     private $cachePool;
+    private $painter;
 
-    public function __construct(Downloader $downloader, Formatter $formatter, PoolInterface $cachePool)
+    public function __construct(Downloader $downloader, Formatter $formatter, PoolInterface $cachePool, TransparentImagePainter $painter)
     {
         $this->downloader = $downloader;
         $this->formatter = $formatter;
         $this->cachePool = $cachePool;
+        $this->painter = $painter;
     }
 
-    function fetchSkin($username, $format = true) {
+    function fetchTransparent($sizeX, $sizeY) {
+        $cacheItem = $this->cachePool->getItem('transparents', $sizeX, $sizeY);
+
+        $data = $cacheItem->get();
+        if($cacheItem->isMiss()) {
+            $cacheItem->lock();
+
+            $data = $this->painter->getImage($sizeX, $sizeY);
+            $cacheItem->set($data);
+        }
+
+        return $data;
+    }
+
+    function fetchSkin($username, $format = true, $error = false) {
         $cacheItem = $this->cachePool->getItem('skins/skin', $username);
 
         $data = $cacheItem->get();
@@ -28,15 +46,21 @@ class SkinFetcher {
         if($cacheItem->isMiss()) {
             $cacheItem->lock();
 
-            $data = $this->downloader->downloadSkin($username);
-
-            $cacheItem->set($data);
+            //attempt to fetch the data
+            //if failed throw error or return a transparent image based on $error
+            try {
+                $data = $this->downloader->downloadSkin($username);
+                $cacheItem->set($data);
+            } catch (DownloadException $ex) {
+                if($error) { throw $ex; }
+                $data = $this->fetchTransparent(64, 32);
+            }
         }
 
         return $format ? $this->formatter->format($data) : $data;
     }
 
-    function fetchHelm($username, $size, $format = true) {
+    function fetchHelm($username, $size, $format = true, $error = false) {
         $cacheItem = $this->cachePool->getItem('skins/helm', $username, $size);
 
         $data = $cacheItem->get();
@@ -44,14 +68,20 @@ class SkinFetcher {
         if($cacheItem->isMiss()) {
             $cacheItem->lock();
 
+            //attempt to fetch the data
+            //if failed throw error or return a transparent image based on $error
+            try {
             $data = $this->downloader->downloadHelm($username, $size);
-
-            $cacheItem->set($data);
+                $cacheItem->set($data);
+            } catch (DownloadException $ex) {
+                if($error) { throw $ex; }
+                $data = $this->fetchTransparent($size, $size);
+            }
         }
         return $format ? $this->formatter->format($data) : $data;
     }
 
-    function fetchHead($username, $size, $format = true)
+    function fetchHead($username, $size, $format = true, $error = false)
     {
         $cacheItem = $this->cachePool->getItem('skins/skin/', $username);
 
@@ -60,9 +90,15 @@ class SkinFetcher {
         if($cacheItem->isMiss()) {
             $cacheItem->lock();
 
-            $data = $this->downloader->downloadHead($username, $size);
-
-            $cacheItem->set($data);
+            //attempt to fetch the data
+            //if failed throw error or return a transparent image based on $error
+            try {
+                $data = $this->downloader->downloadHead($username, $size);
+                $cacheItem->set($data);
+            } catch (DownloadException $ex) {
+                if($error) { throw $ex; }
+                $data = $this->fetchTransparent($size, $size);
+            }
         }
         return $format ? $this->formatter->format($data) : $data;
     }
